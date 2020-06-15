cc6d87ec3d040a09d63a92c9a72fbea0c53602b9
Adrian <adrianlski@gmail.com>
2020-06-15 21:30:30 +0200

    Done

---
+++ StarChart/Controllers/CellestialObjectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    public class CellestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CellestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
